45878d0e127568f2f08c0b410706352d5ce2964d
LilAngelBruh <angel117tutu@gmail.com>
2025-01-23 20:11:17 -0700

    refactor: Eliminacion de Carpeta Drizzle

--- drizzle/0000_numerous_adam_destine.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `usuarios` (
-	`usuarioId` varchar(36) NOT NULL,
-	`matricula` varchar(8) NOT NULL,
-	`password` varchar(8) NOT NULL,
-	CONSTRAINT `usuarios_usuarioId` PRIMARY KEY(`usuarioId`)
-);
