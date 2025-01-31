5e7059abb4bac2d573b5ef11e817b0522513befa
alexis <angel117tutu@gmail.com>
2025-02-01 01:14:00 +0700

    feat: Schema Usuario con Genero

--- src/Database/Infrastructure/Drizzle/schemas/UsuarioSchema.ts
+++ src/Database/Infrastructure/Drizzle/schemas/UsuarioSchema.ts
@@ -1,7 +1,8 @@
-import { mysqlTable, varchar } from 'drizzle-orm/mysql-core';
+import { mysqlEnum, mysqlTable, varchar } from 'drizzle-orm/mysql-core';
 export const UsuariosSchema = mysqlTable('usuarios', {
   usuarioId: varchar({ length: 36 }).primaryKey(),
   nombre: varchar({ length: 50 }).notNull(),
   matricula: varchar({ length: 8 }).notNull(),
   password: varchar({ length: 8 }).notNull(),
+  genero: mysqlEnum(['M', 'F']).notNull(),
 });
