934826eb8f1ff9c2dea06fb75da695b8c20a1046
LilAngelBruh <angel117tutu@gmail.com>
2025-02-05 14:26:20 -0700

    style: formato

--- src/Empleados/Domain/DiasdeVacaciones.ts
+++ src/Empleados/Domain/DiasdeVacaciones.ts
@@ -1,23 +1,23 @@
-import { InvalidDataException } from "@/src/Shared/Domain/Exceptions/InvalidDataException";
+import { InvalidDataException } from '@/src/Shared/Domain/Exceptions/InvalidDataException';
 
 export class DiasdeVacaciones {
-    public value: number
-    public constructor(value:number){
-        this.validarVacaciones(value)
-        this.value=value
+  public value: number;
+  public constructor(value: number) {
+    this.validarVacaciones(value);
+    this.value = value;
+  }
+  private validarVacaciones(value: number): void {
+    if (!value)
+      throw new InvalidDataException({
+        message: 'El Numero de dias de vacaciones es necesario',
+        campo: 'DiasdeVacaciones',
+      });
+    if (isNaN(value)) {
+      throw new InvalidDataException({
+        message: 'Los dias de vacaciones tiene que ser un numero',
+        campo: 'DiasdeVacaciones',
+        data: value,
+      });
     }
-    private validarVacaciones(value:number):void{
-         if (!value)
-              throw new InvalidDataException({
-                message: 'El Numero de dias de vacaciones es necesario',
-                campo: 'DiasdeVacaciones',
-              });
-            if (isNaN(value)) {
-              throw new InvalidDataException({
-                message: 'Los dias de vacaciones tiene que ser un numero',
-                campo: 'DiasdeVacaciones',
-                data: value,
-              });
-            }
-    }
+  }
 }
--- src/Empleados/Domain/NombreEmpleado.ts
+++ src/Empleados/Domain/NombreEmpleado.ts
@@ -1,32 +1,32 @@
-import { InvalidDataException } from "@/src/Shared/Domain/Exceptions/InvalidDataException";
+import { InvalidDataException } from '@/src/Shared/Domain/Exceptions/InvalidDataException';
 
-export class NombreEmplado{
-    public value: string;
-    public constructor(value:string){
-      this.validarNombre(value);
-      this.value= value
-    }
+export class NombreEmplado {
+  public value: string;
+  public constructor(value: string) {
+    this.validarNombre(value);
+    this.value = value;
+  }
 
-    private  validarNombre(value:string):void {
-          if (!value)
-              throw new InvalidDataException({
-                message: 'El Nombre es necesario',
-                campo: 'nombre',
-                data: this.value,
-              });
-        
-            if (value.length < 3)
-              throw new InvalidDataException({
-                message: 'El Nombre debe ser al menos 3 caracteres',
-                campo: 'nombre',
-                data: this.value,
-              });
-        
-            if (value.length > 50)
-              throw new InvalidDataException({
-                message: 'El Nombre debe ser al menos 50 caracteres',
-                campo: 'nombre',
-                data: this.value,
-              });
-    }
+  private validarNombre(value: string): void {
+    if (!value)
+      throw new InvalidDataException({
+        message: 'El Nombre es necesario',
+        campo: 'nombre',
+        data: this.value,
+      });
+
+    if (value.length < 3)
+      throw new InvalidDataException({
+        message: 'El Nombre debe ser al menos 3 caracteres',
+        campo: 'nombre',
+        data: this.value,
+      });
+
+    if (value.length > 50)
+      throw new InvalidDataException({
+        message: 'El Nombre debe ser al menos 50 caracteres',
+        campo: 'nombre',
+        data: this.value,
+      });
+  }
 }
