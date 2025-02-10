b4523998072101a0290c5c98043f57e051812516
Josue Vega <josuevega4567@gmail.com>
2025-02-09 20:10:52 -0700

    feat: :white_check_mark: SolicitudDescanso

---
+++ src/DescansoSolicitud/Domain/Entities/DescansoSolicitudFechaFinal.ts
@@ -0,0 +1,27 @@
+import { InvalidDataException } from '@/src/Shared/Domain/Exceptions/InvalidDataException';
+
+export class DescansoSolicitudFechaFinal {
+  public value: Date;
+  public constructor(value: Date) {
+    this.ensureIsValid(value)
+    this.value = value;
+  }
+  private ensureIsValid(value: Date): void {
+    const regex = /^\d{4}-\d{2}-\d{2}$/;
+    const dateString = value.toISOString().split('T')[0];
+    if (!value) {
+      throw new InvalidDataException({
+        message: 'Favor de Ingresar Una Fecha',
+        campo: 'DescansoFechaFinal',
+      });
+    }
+
+    if (!regex.test(dateString)) {
+      throw new InvalidDataException({
+        message: 'favor la fecha en el formato correcto YYYY/MM/DD',
+        campo: 'DescansoFechaFinal',
+        data: dateString,
+      });
+    }
+  }
+}
