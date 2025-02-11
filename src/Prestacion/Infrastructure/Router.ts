20d5b4b6d98d917c0f4c6beb6e636fd2fa4f3285
LilAngelBruh <angel117tutu@gmail.com>
2025-02-10 19:16:38 -0700

    feat: Controller y Router para prestaciones

---
+++ src/Prestacion/Infrastructure/Router.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+//import { PrestacionController } from './Controllers';
+
+//const controller = new PrestacionController();
+const router = Router();
+
+export { router as PrestacionRouter };
