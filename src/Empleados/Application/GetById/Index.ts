a5369c2bd4ceb26df802fff59dc976f1d1528f05
LilAngelBruh <angel117tutu@gmail.com>
2025-02-16 20:24:03 -0700

    style: Formateo

--- src/Empleados/Application/GetById/Index.ts
+++ src/Empleados/Application/GetById/Index.ts
@@ -1,18 +1,18 @@
-
-import { NotFoundException } from "@/src/Shared/Domain/Exceptions/NotFound";
-import { EmpleadoNumero } from "../../Domain/Entities/EmpleadoNumero";
-import { EmpleadoRepository } from "../../Domain/Entities/EmpleadoRepository";
-import { EmpleadoPrimitive } from "../../Domain/Interfaces/EmpleadoPrimitive";
+import { NotFoundException } from '@/src/Shared/Domain/Exceptions/NotFound';
+import { EmpleadoNumero } from '../../Domain/Entities/EmpleadoNumero';
+import { EmpleadoRepository } from '../../Domain/Entities/EmpleadoRepository';
+import { EmpleadoPrimitive } from '../../Domain/Interfaces/EmpleadoPrimitive';
 
 export class GetEmpleadoById {
-    public constructor(private readonly empleadoRepo: EmpleadoRepository) {}
-    
-    public async run(id: number): Promise<EmpleadoPrimitive | null> {
-        const empleadoId = new EmpleadoNumero(id);
-        const empleado = await this.empleadoRepo.getById(empleadoId.value);
+  public constructor(private readonly empleadoRepo: EmpleadoRepository) {}
 
-        if (!empleado) throw new NotFoundException({ message: 'El empleado no existe', campo: 'id', data: id });
+  public async run(id: number): Promise<EmpleadoPrimitive | null> {
+    const empleadoId = new EmpleadoNumero(id);
+    const empleado = await this.empleadoRepo.getById(empleadoId.value);
 
-        return empleado;  
-    }
+    if (!empleado)
+      throw new NotFoundException({ message: 'El empleado no existe', campo: 'id', data: id });
+
+    return empleado;
+  }
 }
